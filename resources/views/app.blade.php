f39b674f9a7daa80686b5c27c9cad5adfcd81050
synelnoor <synelnoor@gmail.com>
2022-04-25 15:15:33 +0700

    first commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+  <div class="container mx-auto">
+
+    <table class="shadow-lg bg-white">
+      <tr>
+        <th class="bg-blue-100 border text-left px-8 py-4">Company</th>
+        <th class="bg-blue-100 border text-left px-8 py-4">Contact</th>
+        <th class="bg-blue-100 border text-left px-8 py-4">Country</th>
+      </tr>
+      <tr>
+        <td class="border px-8 py-4">Alfreds Futterkiste</td>
+        <td class="border px-8 py-4">Dante Sparks</td>
+        <td class="border px-8 py-4">Italy</td>
+      </tr>
+      <tr>
+        <td class="border px-8 py-4">Centro comercial Moctezuma</td>
+        <td class="border px-8 py-4">Neal Garrison</td>
+        <td class="border px-8 py-4">Spain</td>
+      </tr>
+      <tr>
+        <td class="border px-8 py-4">Ernst Handel</td>
+        <td class="border px-8 py-4">Maggie O'Neill</td>
+        <td class="border px-8 py-4">Austria</td>
+      </tr>
+    </table>
+  </div>
+</body>
+<!-- JS -->
+<script src="{{ asset('js/app.js') }}"></script>
+</html>
